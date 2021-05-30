b159ff665ee8c2407a1b102318b077e80fbddbf4
Ethan Lipnik <ethanlipnik@gmail.com>
2021-05-30 16:16:51 -0400

    Fixed some gradient crashes

--- pages/AppPreview.js
+++ pages/AppPreview.js
@@ -15,7 +15,7 @@ export default class AppPreview extends React.Component {
     render() {
         return (
             <div className="w-full h-xl bg-gray-200 dark:bg-gray-600 flex justify-center items-center rounded">
-                <div className={`bg-gradient-to-b from-${this.state.gradient[0]} to-${this.state.gradient[1]} w-full h-xl flex relative z-0 transform  transition-transform duration-500 ease-in-out ${this.state.reversedRotation ? "rotate-2 hover:-rotate-1" : "-rotate-2 hover:rotate-1"} rounded shadow p-8`}>
+                <div className={`bg-gradient-to-b from-${this.state.gradient ? this.state.gradient[0] : 'bg-blue-300'} to-${this.state.gradient ? this.state.gradient[1] : 'bg-blue-500'} w-full h-xl flex relative z-0 transform  transition-transform duration-500 ease-in-out ${this.state.reversedRotation ? "rotate-2 hover:-rotate-1" : "-rotate-2 hover:rotate-1"} rounded shadow p-8`}>
                     <div className={`flex w-full transform transition-transform ${this.state.reversedRotation ? "-rotate-2" : "rotate-2"}`}>
                         <div className="bg-white dark:bg-gray-700 rounded shadow w-1/3 h-full p-4 z-10">
                             <img src={this.state.imageURL} alt={this.state.appName} width={140} height={140} className="mx-auto rounded shadow m-5" />
