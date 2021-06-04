9e59d0591013a4d30b2e61f12753ee0b45de4cde
Ethan Lipnik <ethanlipnik@gmail.com>
2021-06-04 10:37:57 -0400

    Began adding a popover

--- pages/AppView.js
+++ pages/AppView.js
@@ -1,5 +1,8 @@
 import * as React from 'react'
 import DetailView from './Views/DetailView'
+import { Fragment } from 'react'
+import { Menu, Transition } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/solid'
 
 export default class AppView extends React.Component {
     imageURL = (path) => {
@@ -9,6 +12,7 @@ export default class AppView extends React.Component {
     render() {
         return (
             <div className="container mx-auto md:px-40 px-5 py-10 space-y-20">
+                <AppPopover />
                 <div className="space-y-16">
                     <img src={this.imageURL(this.props.icon ? this.props.icon : "")} width={330} height={330} className={`rounded-app shadow${this.props.title ? "-" + this.props.title.toLowerCase() : ""} mx-auto`} />
                     <div className="space-y-4 p-8">
@@ -32,4 +36,111 @@ export default class AppView extends React.Component {
             </div>
         )
     }
+}
+
+function AppPopover() {
+    return (
+        <Menu as="div" className="relative inline-block text-left shadow">
+            {({ open }) => (
+                <>
+                    <div>
+                        <Menu.Button className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
+                            Apps
+              <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+                        </Menu.Button>
+                    </div>
+
+                    <Transition
+                        show={open}
+                        as={Fragment}
+                        enter="transition ease-out duration-100"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                    >
+                        <Menu.Items
+                            static
+                            className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none"
+                        >
+                            <div className="py-1">
+                                <Menu.Item>
+                                    {({ active }) => (
+                                        <a
+                                            href="#"
+                                            className={classNames(
+                                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                'block px-4 py-2 text-sm'
+                                            )}
+                                        >
+                                            Neptune
+                                        </a>
+                                    )}
+                                </Menu.Item>
+                                <Menu.Item>
+                                    {({ active }) => (
+                                        <a
+                                            href="#"
+                                            className={classNames(
+                                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                'block px-4 py-2 text-sm'
+                                            )}
+                                        >
+                                            Syndromi
+                                        </a>
+                                    )}
+                                </Menu.Item>
+                                <Menu.Item>
+                                    {({ active }) => (
+                                        <a
+                                            href="#"
+                                            className={classNames(
+                                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                'block px-4 py-2 text-sm'
+                                            )}
+                                        >
+                                            Kairos
+                                        </a>
+                                    )}
+                                </Menu.Item>
+                            </div>
+                            <div className="py-1">
+                                <Menu.Item>
+                                    {({ active }) => (
+                                        <a
+                                            href="#"
+                                            className={classNames(
+                                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                'block px-4 py-2 text-sm'
+                                            )}
+                                        >
+                                            Sakka
+                                        </a>
+                                    )}
+                                </Menu.Item>
+                                <Menu.Item>
+                                    {({ active }) => (
+                                        <a
+                                            href="#"
+                                            className={classNames(
+                                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                'block px-4 py-2 text-sm'
+                                            )}
+                                        >
+                                            Kano
+                                        </a>
+                                    )}
+                                </Menu.Item>
+                            </div>
+                        </Menu.Items>
+                    </Transition>
+                </>
+            )}
+        </Menu>
+    )
+}
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
 }
--- pages/apps/neptune/index.js
+++ pages/apps/neptune/index.js
@@ -34,9 +34,6 @@ export default class NeptunePage extends React.Component {
                     <meta property="og:url" content="https://ethanlipnik.com/apps/neptune" />
                     <meta property="og:image" content="https://ethanlipnik.com/apps/neptune/android-chrome-512x512.png" />
                 </Head>
-                <Link href={"/apps/neptune/privacy"}>
-                    <h1>Privacy</h1>
-                </Link>
                 <AppView
                     title="Neptune"
                     subtitle="Out of this World"
