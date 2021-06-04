0f7c9449e4ba5ac0f54ff1b87d2febfda5957b8e
Ethan Lipnik <ethanlipnik@gmail.com>
2021-06-04 10:03:56 -0400

    Improvements to privacy policies

--- pages/PrivacyView.js
+++ pages/PrivacyView.js
@@ -40,7 +40,7 @@ class DetailView extends React.Component {
 
     render() {
         return (
-            <div className="bg-gray-200 dark:bg-gray-600 justify-center items-center rounded my-8">
+            <div className={`bg-gray-200 dark:bg-gray-600 justify-center items-center rounded my-8  transform transition-transform duration-500 ease-in-out ${this.state.reversedRotation ? "-rotate-2" : "rotate-2"}`}>
                 <div className={`bg-gradient-to-b from-${(this.state.gradient ? this.state.gradient[0] : 'bg-blue-300')} to-${(this.state.gradient ? this.state.gradient[1] : 'bg-blue-500')} w-full relative z-0 transform transition-transform duration-500 ease-in-out ${this.state.reversedRotation ? "rotate-2 hover:-rotate-1" : "-rotate-2 hover:rotate-1"} rounded shadow${this.state.appTitle ? "-" + this.state.appTitle.toLowerCase() : ""} p-8`}>
                     <div className={`w-full space-y-5 transform transition-transform ${this.state.reversedRotation ? "-rotate-2" : "rotate-2"}`}>
                         <h1 className="w-full text-3xl text-white font-bold">{this.state.component.title}</h1>
